affaf22199bf4f00ee92008b8094756414d97e64
anjaliasha123 <anjaliasha95@gmail.com>
2022-11-24 11:09:47 -0600

    added animations

--- src/components/Die.js
+++ src/components/Die.js
@@ -6,8 +6,8 @@ class Die extends Component{
     render(props){
         return(
             <div className='app-dice'>
-                <i className={"fa-solid fa-dice-"+this.props.s1}></i>
-                <i className={"fa-solid fa-dice-"+this.props.s2}></i>
+                <i className={"fa-solid fa-dice-"+this.props.s1} data-shake={this.props.status}></i>
+                <i className={"fa-solid fa-dice-"+this.props.s2} data-shake={this.props.status}></i>
             
             </div>
         )
--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -9,7 +9,8 @@ class RollDice extends Component{
         super(props);
         this.state= {
             num1: 1,
-            num2: 1
+            num2: 1,
+            rolling: false
         }
         this.updateNums = this.updateNums.bind(this);
         dieList = ["one","two","three","four","five","six"];
@@ -19,16 +20,26 @@ class RollDice extends Component{
         let rand2 = Math.floor(Math.random()*6 + 1);
         this.setState({
             num1: rand1,
-            num2: rand2
+            num2: rand2,
+            rolling: true
         });
+        setTimeout(()=>{
+            this.setState({rolling: false})
+            
+        },1000);
         console.log(this.state.num1, this.state.num2)
     }
 
     render(){
         return(
             <div className='app-roll-dice'>
-                <Die s1={dieList[this.state.num1 - 1]} s2={dieList[this.state.num2 - 1]}/>
-                <button className='roll-dice' onClick={this.updateNums}>Roll Dice</button>
+                <Die 
+                s1={dieList[this.state.num1 - 1]} 
+                s2={dieList[this.state.num2 - 1]} 
+                status={this.state.rolling}/>
+                <button className='roll-dice' onClick={this.updateNums} disabled={this.state.rolling}>
+                    {this.state.rolling? 'Rolling...' : 'Roll Dice!'}
+                </button>
             </div>
         )
     }
